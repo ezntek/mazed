3e6edc0920ae5a750a67f20237f990546580b900
easontek2398 <easontekreviews@icloud.com>
2022-08-04 11:10:24 +0800

    somewhat add ball

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/tek967/mazed
 go 1.18
 
 require (
-	github.com/gen2brain/raylib-go/raylib v0.0.0-20220702153720-2ba84634ed1e // indirect
-	github.com/tek967/rgbapalette v0.0.0-20220724015502-5cd92fbe69d5 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20220702153720-2ba84634ed1e
+	github.com/tek967/rgbapalette v0.0.0-20220724015502-5cd92fbe69d5
 )
--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	rl "github.com/gen2brain/raylib-go/raylib"
+	"github.com/tek967/mazed/src/ball"
 	pl "github.com/tek967/rgbapalette"
 )
 
@@ -10,10 +11,19 @@ const (
 	height int32 = 864
 )
 
-func draw() {
-	rl.DrawText("it works!", 20, 20, 20, rl.Black)
+var theBall *ball.Ball = ball.New(rl.NewVector2(50, 50), 20, rl.Black)
+
+func beforeLoopSetup() {
+	rl.InitWindow(width, height, "wow game")
 }
+
+func draw() {
+	theBall.Draw()
+}
+
 func update() {
+	theBall.Update(width, height)
+
 	rl.BeginDrawing()
 	rl.ClearBackground(pl.Palette["verylightgray"])
 	draw()
@@ -21,9 +31,7 @@ func update() {
 }
 
 func main() {
-	rl.InitWindow(width, height, "wow game")
-	rl.SetTargetFPS(60)
-
+	beforeLoopSetup()
 	for !rl.WindowShouldClose() {
 		update()
 	}
